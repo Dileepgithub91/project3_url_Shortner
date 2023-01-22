5b4dc00305c07341239ec6732c520dfbbc38449d
Dileep <dileepkumar919996@gmail.com>
2023-01-22 16:11:51 +0530

    final url_Shoetner

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,75 +1,79 @@
 const urlModel = require('../modules/urlModel')
 const shortid = require('shortid')
 const redis = require("redis")
-const validUrl = require('validator')
+const axios = require('axios')
+const validator = require('validator')
 const { promisify } = require("util")
 
 
 //1. Connect to the redis server
 
-const redisClient = redis.createClient(
-    18345,
-    "redis-18345.c264.ap-south-1-1.ec2.cloud.redislabs.com",
-    { no_ready_check: true }
-);
-redisClient.auth("RXGCwJUfsFLEWSsJIsiiFJCjdJFNGE0m", function (err) {
-    if (err) throw err;
-});
-
-redisClient.on("connect", async function () {
-    console.log("Connected to Redis..");
-});
-
+const client = redis.createClient({
+    url: 'redis://default:W6hFYmk66yEZdM6Yi7yrTSIi8NfrNRrR@redis-15210.c212.ap-south-1-1.ec2.cloud.redislabs.com:15210'
+})
+client.on('error', (err) => console.log('Redis Client Error', err))
+console.log("Connected to Redis..")
 
 //2. Prepare the functions for each command
 
-const SET_ASYNC = promisify(redisClient.SETEX).bind(redisClient);
-const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
+const SET_ASYNC = promisify(client.SETEX).bind(client);
+const GET_ASYNC = promisify(client.GET).bind(client);
 
 
-
-const url = async function (req, res) {
+const url = async function(req, res) {
     try {
         let data = req.body
         let { longUrl } = data
+
         if (Object.keys(data).length == 0) return res.status(400).send({ status: false, msg: "Request Body cant be empty" })
+        if (!Object.keys(data).includes('longUrl')) return res.status(400).send({ status: false, msg: "'Url' should be there in request body" })
+        longUrl = longUrl.trim()
+        if (longUrl == "") return res.status(400).send({ status: false, msg: "Enter 'Url' in request body" })
+        if (Object.keys(data).length > 1) return res.status(400).send({ status: false, msg: "Enter 'Url' only in request body" })
+        if (!validator.isURL(longUrl)) return res.status(400).send({ status: false, msg: `The URL format is not valid` })
 
-        if (!Object.keys(data).includes('longUrl')) return res.status(400).send({ status: false, msg: "'longUrl' should be there in request body" })
-        if (Object.keys(data).length > 1) return res.status(400).send({ status: false, msg: "Enter 'longUrl' only in request body" })
+        const response = await axios.get(longUrl)
+            .then(() => longUrl)
+            .catch(() => null)
+        if (!response) return res.status(404).send({ status: false, msg: `The URL ${longUrl} is not valid` })
 
-        // let LinkFormat = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%.\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%\+.~#?&\/=]*)$/
-        if (!validUrl.isURL(longUrl)) return res.status(400).send({ status: false, msg: "Please Enter Valid Url" })
+
+        let cachedProfileData = await GET_ASYNC(`${data.longUrl}`)
+        if (cachedProfileData) return res.status(200).send({ status: true, data: JSON.parse(cachedProfileData) })
+
         const findUrl = await urlModel.findOne({ longUrl: longUrl }).select({ longUrl: 1, shortUrl: 1, urlCode: 1, _id: 0 })
-        if (findUrl) { return res.status(200).send({ status: true, data: { longUrl: findUrl.longUrl, shortUrl: findUrl.shortUrl, urlCode: findUrl.urlCode } }) };
-        
+        if (findUrl) {
+            await SET_ASYNC(`${data.longUrl}`, 86400, JSON.stringify(findUrl))
+            return res.status(200).send({ status: true, data: findUrl })
+        };
+
         let shortUrlCode = shortid.generate()
         let baseUrl = `${req.protocol}://${req.get('host')}/`
         data.urlCode = shortUrlCode
         data.shortUrl = baseUrl + shortUrlCode
         let { shortUrl, urlCode } = data
         let createData = await urlModel.create(data)
-        await SET_ASYNC(`${shortUrlCode}`, 50,  JSON.stringify(data))
+        await SET_ASYNC(`${shortUrlCode}`, 86400, JSON.stringify(data))
+        await SET_ASYNC(`${data.longUrl}`, 86400, JSON.stringify(data))
         return res.status(201).send({ status: true, data: { longUrl, shortUrl, urlCode } })
     } catch (error) {
-        return res.send({ errorType: error.name, errorMsg: error.message })
+        return res.status(500).send({ errorType: error.name, errorMsg: error.message })
     }
 
 }
 
-const getUrl = async function (req, res) {
+const getUrl = async function(req, res) {
     try {
-        let cahcedProfileData = await GET_ASYNC(`${req.params.urlCode}`)
-        if (cahcedProfileData) {
-            return res.status(302).redirect(JSON.parse(cahcedProfileData).shortUrl)
-        }
-        else {
-            let getData = await urlModel.findOne({ urlCode: req.params.urlCode }).select({ urlCode: 1, shortUrl: 1, longUrl: 1, _id: 0 })
-            if (!getData) return res.status(400).send({ status: false, msg: "Page Not found" })
-            await SET_ASYNC(`${req.params.urlCode}`, 10,  JSON.stringify(getData))
-            return res.status(302).redirect(JSON.parse(getData).shortUrl)
-        }
+        let cachedProfileData = await GET_ASYNC(`${req.params.urlCode}`)
+        if (cachedProfileData) return res.status(302).redirect(JSON.parse(cachedProfileData).longUrl)
+
+        let getData = await urlModel.findOne({ urlCode: req.params.urlCode }).select({ urlCode: 1, shortUrl: 1, longUrl: 1, _id: 0 })
+        if (!getData) return res.status(400).send({ status: false, msg: "Page Not found" })
+        await SET_ASYNC(`${req.params.urlCode}`, 86400, JSON.stringify(getData))
+        return res.status(302).redirect(getData.longUrl)
+
     } catch (error) {
-        return res.send({ errorType: error.name, errorMsg: error.message })
+        return res.status(500).send({ errorType: error.name, errorMsg: error.message })
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 const express = require('express')
 const route = require('./routes/route')
 const mongoose = require('mongoose')
+const urlModel = require('./modules/urlModel')
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 mongoose.set('strictQuery', false)
-mongoose.connect("mongodb+srv://group11Database:group11Database@urlshort3.gtvadgh.mongodb.net/group11Database", {
-    useNewUrlParser: true
-})
+mongoose.connect("mongodb+srv://dileepkm:L3cuCdGwQQWTF3Hs@cluster0.iqkms8u.mongodb.net/test", {
+        useNewUrlParser: true
+    })
     .then(() => console.log("MongoDB is connected"))
     .catch(err => console.log(err))
 
 app.use("/", route)
 
-app.listen(3000, function () {
+app.listen(3000, function() {
     console.log("Express app running on port 3000")
 })
